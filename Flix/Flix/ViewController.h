62acd6ef8938ab234b9ea82ec451bccfd6d86e37
hechen.gao <zerelghc@gmail.com>
2020-07-25 14:08:33 -0700

    add basic Flix

---
+++ Flix/Flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flix
+//
+//  Created by Hechen Gao on 7/25/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
